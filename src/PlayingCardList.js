24aa52bf86ea575fb39fd65c8abe009f30a23576
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-20 18:33:53 -0400

    Updated PlayingCardList to utilize useAxios hook for data fetching.
    - Integrated useAxios hook to fetch card data from the API.
    - Modified card rendering to correctly map over fetched card data.
    - Enhanced button interaction to fetch and display a new card upon click.

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
-import { v4 as uuid } from "uuid";
-import axios from "axios";
+import React from "react";
+import { useAxios } from "./hooks";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 
-/* Renders a list of playing cards.
- * Can also add a new card at random. */
-function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+function PlayingCardList() {
+  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
@@ -22,13 +14,11 @@ function CardTable() {
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+          <PlayingCard key={cardData.id} front={cardData.image} />
         ))}
       </div>
     </div>
   );
 }
 
-CardTable.defaultProps = {};
-
-export default CardTable;
+export default PlayingCardList;
