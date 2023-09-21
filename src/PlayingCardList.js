c93f2d76158f9c752e76c50d182cefeabf872951
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-20 21:21:38 -0400

    Adjust PlayingCardList to utilize updated useAxios hook, add delete button, resolve eslint error
    Adjust PlayingCardList to utilize updated useAxios hook
    Resolve eslint error related to clearCards function in PlayingCardList

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -4,18 +4,19 @@ import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 
 function PlayingCardList() {
-  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+  const [cards, addCard, clearCards] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
 
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+      <button onClick={() => addCard()}>Add a playing card!</button>
+      <button onClick={() => clearCards()}>Delete all cards!</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.image} />
-        ))}
+      {cards.map(cardData => (
+  <PlayingCard key={cardData.cards[0].code} front={cardData.cards[0].image} />
+))}
       </div>
     </div>
   );
