affeb2ccfee817a7bc15790dbb293e5828aee42a
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-20 21:31:04 -0400

    Update PokemonCard. with correct image

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import backOfCard from "./back.png";
+import backOfCard from "./Pokemoncardback.jpeg";
 import "./PokemonCard.css";
 import { useFlip } from "./hooks";  // Import the useFlip hook
 
